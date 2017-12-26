1468744178a7d39565260469f1a93938f46a6bab
nielyd <nielong2014@gmail.com>
2017-12-26 09:47:42 +0800

    初试成功。不使用 def 文件；不考虑 c++ 重载；不考虑函数的调用方式 __cdecl / __stdcall / _fastcall~

--- TestDll/add.cpp
+++ TestDll/add.cpp
@@ -1,11 +1,25 @@
 #include <string>
 
-int add(const int a, const int b)
+int _add(const int a, const int b)
 {
 	return a + b;
 }
 
-std::string add(const std::string& str1, const std::string& str2)
+extern "C" __declspec(dllexport) int add(const int a, const int b)
 {
-	return str1 + str2;
+	return _add(a, b);
 }
+
+__declspec(dllexport) int sub(const int a, const int b)
+{
+	return a-b;
+}
+// c
+//1    0 000110E1 add = @ILT+220(_add)
+// c++  
+//1    0 00011208 ? add@@YAHHH@Z = @ILT+515(? add@@YAHHH@Z)
+
+//std::string add(const std::string& str1, const std::string& str2)
+//{
+//	return str1 + str2;
+//}
--- TestDll/main.cpp
+++ TestDll/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
-#include "add.h"
+#include <Windows.h>
+#include <cassert>
+#include <winuser.h>
+// 1. 先考虑不使用 def 文件。这种方式貌似是后出现的
+// 2. 暂时也不考虑重载，c 和 C++ 在编译期间命名的区别
+// 3. 函数的调用方式相关__cdecl / __stdcall / _fastcall 几个方式的修饰符号也不相同
+
+//typedef  int(_cdecl *ADD)(const int, const int);
+typedef  int( *ADD)(const int, const int);
 
 int main(int argc, char* argv[])
 {
-	std::cout << add("the result of", "（add(1, 2)）:") << add(1, 2) << std::endl;
+	HINSTANCE hInstance = LoadLibrary("add.dll");
+	assert(hInstance);
+
+	ADD add = (ADD)GetProcAddress(hInstance, "add"); // c 的函数名
+	//ADD add = (ADD)GetProcAddress(hInstance, MAKEINTRESOURCE(2)); //it's ok
+	if (add)
+		std::cout << "the result of（add(1, 2)）:" << add(1, 2) << std::endl;
+
+	//ADD sub = (ADD)GetProcAddress(hInstance, "sub");  // ERR
+	ADD sub = (ADD)GetProcAddress(hInstance, "?sub@@YAHHH@Z");  // c++ 的函数名
+	//ADD sub = (ADD)GetProcAddress(hInstance, MAKEINTRESOURCE(1)); //it's ok
+	if (sub)
+		std::cout << "the result of（sub(1, 2)）:" << sub(1, 2) << std::endl;
+
+	FreeLibrary(hInstance);
 	return 0;
 }
+
+// 似乎用不到头文件
