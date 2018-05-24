e830674c82a22217014a25b0f0fa223a1ab9ebe6
nielyd <nielong2014@gmail.com>
2018-05-24 17:06:51 +0800

    关于 #7 issue 的试验

--- TestDll/main.cpp
+++ TestDll/main.cpp
@@ -4,12 +4,14 @@
 #include <winuser.h>
 #include <string>
 #include "../add/add.h"
+#include "../sub/sub.h"
 // 1. 先考虑不使用 def 文件。这种方式貌似有缺陷才出现的 __declspec(dllexport/dllimport)
 // 2. 暂时也不考虑重载，c 和 C++ 在编译期间命名的区别
 // 3. 函数的调用方式相关__cdecl / __stdcall / _fastcall 几个方式的修饰符号也不相同
 
-#pragma comment(lib, "../Debug/add.lib")
-extern "C" __declspec(dllimport) int sub(const int a, const int b);
+#pragma comment(lib, "../Debug/addd.lib")
+#pragma comment(lib, "../Debug/subd.lib")
+//extern "C" __declspec(dllimport) int sub(const int a, const int b);
 
 using namespace std;
 int main(int argc, char* argv[])
@@ -27,5 +29,9 @@ int main(int argc, char* argv[])
         auto str = returnRef();
         cout << str << endl;
     }
-	return 0;
+
+    {
+        cout << "2-1=" << sub(2, 1) << endl;
+    }
+    return 0;
 }
---
+++ sub/sub.cpp
@@ -0,0 +1,7 @@
+#define __ADD_DLL
+#include "sub.h"
+
+int sub(const int a, const int b)
+{
+    return (a - b);
+}
