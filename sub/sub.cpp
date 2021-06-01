e9b71a1ba639aab76bbf3964d6fed598b041266c
niel <nielong@yd.com>
2021-06-01 14:25:13 +0800

    fix bug

--- sub/sub.cpp
+++ sub/sub.cpp
@@ -1,7 +1,8 @@
-#define __ADD_DLL
 #include "sub.h"
 
-int sub(const int a, const int b)
-{
-    return (a - b);
+extern "C" {
+    int sub(const int a, const int b)
+    {
+        return (a - b);
+    }
 }
--- sub/sub.h
+++ sub/sub.h
@@ -1,10 +1,5 @@
 #pragma once
-#ifdef __SUB_DLL
-#define _SUB_API __declspec(dllexport)
-#else
-#define _SUB_API __declspec(dllimport)
-#endif
 
-_SUB_API int sub(const int a, const int b);
-
-#undef  _SUB_API
+extern "C" {
+    int sub(const int a, const int b);
+}
